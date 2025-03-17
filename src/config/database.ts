d9cae196a53938569f210313a4ea3619202bbf7d
Nicolás Bonoris <nicolas.bonoris194136@alumnos.info.unlp.edu.ar>
2025-03-17 10:53:10 -0300

    Fix: database connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { AppError } from "../errors/AppError";
 import { ErrorType } from "../enums/errors";
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://admin:secret@mongo:27017/xlsx-uploader?authSource=admin";
+const MONGO_URI = process.env.MONGO_URI ;
 const isTestEnv = process.env.NODE_ENV === "test";
 
 const MAX_RETRIES = 5;
@@ -10,16 +10,13 @@ const RETRY_DELAY = 5000; // 5 secs
 
 export const connectDB = async () => {
   let attempts = 0;
+  console.log(MONGO_URI)
 
   while (attempts < MAX_RETRIES) {
     try {
       console.log("⏳ Connecting to MongoDB...");
-    await mongoose.connect(MONGO_URI, {
-      connectTimeoutMS: 30000,
-      socketTimeoutMS: 30000,
-      serverSelectionTimeoutMS: 30000
-    });
-    console.log("✅ Connected to MongoDB");
+      await mongoose.connect(MONGO_URI ?? "");
+      console.log("✅ Connected to MongoDB");
       return;
     } catch (error) {
       attempts++;
